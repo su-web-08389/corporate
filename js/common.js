80b8b04406564d5095a0880cc2fb42a84721fcbb
su-web-08389 <yayoi08389@gmail.com>
2026-02-08 14:25:15 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,41 @@
+function animateCount(element) {
+    const target = parseInt(element.getAttribute('date__number'));
+    let current = 0;
+    const duration =1000;
+    const startTime = performance.now();
+
+    function update() {
+        const now = performance.now();
+        const elapsedTime = now - startTime;
+        const progress = Math.min(elapsedTime / duration, 1);
+
+        current = Math.floor(progress * target);
+        element.textContent = current.toLocaleString();
+
+        if (progress < 1) {
+            requestAnimationFrame(update);
+        } else{
+                element.textContent = target.toLocaleString();
+            }
+        }
+        
+        requestAnimationFrame(update);
+    }
+
+    document.addEventListener('DOMContentLoaded',() => {
+        const countElements = document.querySelectorAll('span[date__number]')
+        const observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if(entry.isIntersecting) {
+                    animateCount(entry.target);
+                    observer.unobserve(entry.target);
+                }
+            });
+        }, {
+            threshold: 1.0
+        });
+
+        countElements.forEach(element => {
+            observer.observe(element);
+        });
+    });
